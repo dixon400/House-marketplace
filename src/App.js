e534a706b14f7962f9c380c9a929af11452c6b01
Muyideen Khalifa <dixonmk400@gmail.com>
2022-08-18 23:16:08 +0100

    Build: Create application routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Explore/>}/>
+        <Route path='/login' element={<SignIn/>} />
+        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/offers' element={<Offers/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/forgotpassword' element={<ForgotPassword/>} />
+      </Routes>
+    </Router>
+    {/*NavBar*/}
+    </>
   );
 }
 
