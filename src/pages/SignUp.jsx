1c7e32c2d6b04cbe214450a43e3b35ebbc29bb1d
Muyideen Khalifa <dixonmk400@gmail.com>
2022-08-24 02:28:53 +0100

    Feature: Register a user

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -10,21 +12,43 @@ function SignUp() {
     email: "",
     password: "",
   });
+
   const { name, email, password } = formData;
+
   const navigate = useNavigate();
+
   const onChange = (e) => {
     setFormData((prevState)=>({
       ...prevState,
       [e.target.id]: e.trget.value 
     }))
   };
+
+  const onSubmit = async(e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCred = await createUserWithEmailAndPassword(auth, email, password)
+      const user = userCred.user;
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      navigate('/')
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="pageContainer">
         <header>
           <p className="pageHeader">WELCOME !</p>
         </header>
-        <form>
+        <form onSubmit={onSubmit}>
         <input
             type="text"
             className="nameInput"
